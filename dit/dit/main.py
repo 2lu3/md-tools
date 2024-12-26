874e3fec2d0c842531f94f814c4ff76eaae973a5
2lu3 <hi2lu3@gmail.com>
2024-12-26 18:51:16 +0900

    scopeをmain.py似追加

--- dit/dit/main.py
+++ dit/dit/main.py
@@ -7,6 +7,7 @@ def main():
 
     add.register_subparser(subparser)
     clean.register_subparser(subparser)
+    scope.register_subparser(subparser)
 
     args = parser.parse_args()
     if args.command == "add":
