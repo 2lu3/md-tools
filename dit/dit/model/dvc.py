1a6f851c13a892390c3c303aa1bdfebf9788a2c9
2lu3 <hi2lu3@gmail.com>
2024-12-27 15:30:11 +0900

    updated

--- dit/dit/model/dvc.py
+++ dit/dit/model/dvc.py
@@ -2,24 +2,23 @@ import glob
 import os
 
 from dit.model.extension import Extension
+from dit.model.git import Git
 from dit.model.scope import Scope
 
 
 class DVC:
     def __init__(self):
+        self.git = Git()
         self.scope = Scope()
         self.extension = Extension()
 
     def find_dvc_files(self):
         files = []
         for directory in self.scope.directories:
-            relpath = os.path.relpath(directory, start=os.getcwd())
-            print(os.path.join(relpath, "**", "*.dvc"))
-            print(glob.glob(os.path.join(relpath, "**", "*.dvc"), recursive=True))
+            relpath = os.path.relpath(os.path.join(self.git.root_dir(), directory))
             files.extend(
                 glob.glob(os.path.join(relpath, "**", "*.dvc"), recursive=True)
             )
 
         files = set(files)
         return files
-
