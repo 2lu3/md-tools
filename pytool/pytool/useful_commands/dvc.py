695ff7d40ca97c1d8cb57b0eb37abcc6811b4603
2lu3 <hi2lu3@gmail.com>
2025-02-18 19:31:21 +0900

    dvc add tar

--- pytool/pytool/useful_commands/dvc.py
+++ pytool/pytool/useful_commands/dvc.py
@@ -2,7 +2,7 @@ import subprocess
 import click
 
 def dvc_add(additional_globs=[]):
-    subprocess.run(["dvc", "add", "--glob", "**/*.dcd", "**/*.rst", "**/*.dvl", "**/*.npy", "**/*.pkl", *additional_globs])
+    subprocess.run(["dvc", "add", "--glob", "**/*.dcd", "**/*.rst", "**/*.dvl", "**/*.npy", "**/*.pkl", "**/*.tar", *additional_globs])
 
 @click.command()
 @click.option("--additional_globs", "-a", multiple=True, help="additional globs to add")
