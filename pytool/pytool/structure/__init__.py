cb982bea0124aeec1ea658c775d90c9963742d3c
2lu3 <hi2lu3@gmail.com>
2025-02-21 22:25:51 +0900

    updated many

--- pytool/pytool/structure/__init__.py
+++ pytool/pytool/structure/__init__.py
@@ -1,6 +1,6 @@
 from .get_box_size import get_box_size
 from .residue_distance import residue_distance
-from .align import align, align_trajectory
+from .alignment import align, align_trajectory
 from .atom_distance import atom_distance
 from .rmsd import rmsd, rmsd_trajectory
 from .rmsf import rmsf
--- pytool/pytool/trajectory/__init__.py
+++ pytool/pytool/trajectory/__init__.py
@@ -1,4 +1,4 @@
-from .concat_dcd import main as concat_dcd
-from .reduce_dcd import reduce_dcd
+from .concatenation import main as concat_dcd
+from .sampling import reduce_dcd
 from .sample_trajectory import sample_trajectory
 from .moving_average import moving_average
