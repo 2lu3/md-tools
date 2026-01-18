9bf8a3ddfcf5d21b4a44b62c8646544c8b7526b2
hikaru muto <rainly@MacBook-Pro.local>
2026-01-18 17:25:27 +0900

    refactor(pyproject.toml): remove alignment commands from pyproject.toml to clean up unused entries refactor(structure): move alignment functions to a new trajectory module for better organization refactor(concatenation.py): simplify concat_dcd function by removing unused parameters and main function

--- pytool/pytool/structure/__init__.py
+++ pytool/pytool/structure/__init__.py
@@ -1,6 +1,5 @@
 from .get_box_size import get_box_size
 from .residue_distance import residue_distance
-from .alignment import align, align_trajectory
 from .atom_distance import atom_distance
 from .rmsd import rmsd, rmsd_trajectory
 from .rmsf import rmsf
---
+++ pytool/pytool/trajectory/alignment.py
@@ -0,0 +1,29 @@
+from typing import Optional, Union
+import MDAnalysis as mda
+from MDAnalysis.analysis.align import AlignTraj, alignto
+
+import warnings
+
+warnings.filterwarnings("ignore")
+
+
+def align(
+    mobile: mda.Universe,
+    ref: mda.Universe,
+    output_path: str,
+    select: str = "protein and name CA",
+):
+    alignto(mobile, ref, select=select, weights="mass")
+
+    output = mda.Writer(output_path, mobile.atoms.n_atoms)  # type: ignore
+    output.write(mobile.atoms)
+
+
+def align_trajectory(
+    mobile: mda.Universe,
+    ref: mda.Universe,
+    output_path: str,
+    select: str = "protein and name CA",
+    verbose: bool = True,
+):
+    AlignTraj(mobile, ref, select=select, filename=output_path, verbose=verbose).run()
