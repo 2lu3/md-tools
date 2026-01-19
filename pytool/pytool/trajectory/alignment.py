0e6ee3cbce84feff4455238ea145897820f2f90b
2lu3 <hi2lu3@gmail.com>
2026-01-19 23:32:22 +0900

    refactor(alignment.py): change parameter types from str to mda.Universe for better type safety and clarity fix(alignment.py): simplify alignment logic by directly using mobile and ref parameters

--- pytool/pytool/trajectory/alignment.py
+++ pytool/pytool/trajectory/alignment.py
@@ -8,16 +8,14 @@ warnings.filterwarnings("ignore")
 
 
 def align(
-    mobile: str,
-    ref: str,
+    mobile: mda.Universe,
+    ref: mda.Universe,
     output_path: str,
     select: str = "protein and name CA",
 ):
-    alignto(mda.Universe(mobile), mda.Universe(ref), select=select, weights="mass")
+    alignto(mobile, ref, select=select, weights="mass")
 
-    output = mda.Writer(output_path, mobile.atoms.n_atoms)  # type: ignore
-    output.write(mobile.atoms)
-
+    mobile.atoms.write(output_path)
 
 def align_trajectory(
     mobile: mda.Universe,
